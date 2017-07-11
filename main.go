e4b1ee05a1546dc785abfed6900741bb0fe86c3b
Meghan Jordan <meghanelizabethj@gmail.com>
2017-07-10 20:20:01 -0400

    add gorilla mux datadog apm integration

--- main.go
+++ main.go
@@ -10,6 +10,9 @@ import (
 	"net/http"
 	"strings"
 
+	"github.com/DataDog/dd-trace-go/tracer"
+	"github.com/DataDog/dd-trace-go/tracer/contrib/gorilla/muxtrace"
+	"github.com/gorilla/mux"
 	"github.com/thewhitetulip/Tasks/config"
 	"github.com/thewhitetulip/Tasks/views"
 )
@@ -34,55 +37,58 @@ func main() {
 
 	views.PopulateTemplates()
 
-	//Login logout
-	http.HandleFunc("/login/", views.LoginFunc)
-	http.HandleFunc("/logout/", views.RequiresLogin(views.LogoutFunc))
-	http.HandleFunc("/signup/", views.SignUpFunc)
+	router := mux.NewRouter()
+	muxTracer := muxtrace.NewMuxTracer("my-tasks-app", tracer.DefaultTracer)
 
-	http.HandleFunc("/add-category/", views.RequiresLogin(views.AddCategoryFunc))
-	http.HandleFunc("/add-comment/", views.RequiresLogin(views.AddCommentFunc))
-	http.HandleFunc("/add/", views.RequiresLogin(views.AddTaskFunc))
+	//Login logout
+	muxTracer.HandleFunc(router, "/login/", views.LoginFunc)
+	muxTracer.HandleFunc(router, "/logout/", views.RequiresLogin(views.LogoutFunc))
+	muxTracer.HandleFunc(router, "/signup/", views.SignUpFunc)
+
+	muxTracer.HandleFunc(router, "/add-category/", views.RequiresLogin(views.AddCategoryFunc))
+	muxTracer.HandleFunc(router, "/add-comment/", views.RequiresLogin(views.AddCommentFunc))
+	muxTracer.HandleFunc(router, "/add/", views.RequiresLogin(views.AddTaskFunc))
 
 	//these handlers are used to delete
-	http.HandleFunc("/del-comment/", views.RequiresLogin(views.DeleteCommentFunc))
-	http.HandleFunc("/del-category/", views.RequiresLogin(views.DeleteCategoryFunc))
-	http.HandleFunc("/delete/", views.RequiresLogin(views.DeleteTaskFunc))
+	muxTracer.HandleFunc(router, "/del-comment/", views.RequiresLogin(views.DeleteCommentFunc))
+	muxTracer.HandleFunc(router, "/del-category/", views.RequiresLogin(views.DeleteCategoryFunc))
+	muxTracer.HandleFunc(router, "/delete/", views.RequiresLogin(views.DeleteTaskFunc))
 
 	//these handlers update
-	http.HandleFunc("/upd-category/", views.RequiresLogin(views.UpdateCategoryFunc))
-	http.HandleFunc("/update/", views.RequiresLogin(views.UpdateTaskFunc))
+	muxTracer.HandleFunc(router, "/upd-category/", views.RequiresLogin(views.UpdateCategoryFunc))
+	muxTracer.HandleFunc(router, "/update/", views.RequiresLogin(views.UpdateTaskFunc))
 
 	//these handlers are used for restoring tasks
-	http.HandleFunc("/incomplete/", views.RequiresLogin(views.RestoreFromCompleteFunc))
-	http.HandleFunc("/restore/", views.RequiresLogin(views.RestoreTaskFunc))
+	muxTracer.HandleFunc(router, "/incomplete/", views.RequiresLogin(views.RestoreFromCompleteFunc))
+	muxTracer.HandleFunc(router, "/restore/", views.RequiresLogin(views.RestoreTaskFunc))
 
 	//these handlers fetch set of tasks
-	http.HandleFunc("/", views.RequiresLogin(views.ShowAllTasksFunc))
-	http.HandleFunc("/category/", views.RequiresLogin(views.ShowCategoryFunc))
-	http.HandleFunc("/deleted/", views.RequiresLogin(views.ShowTrashTaskFunc))
-	http.HandleFunc("/completed/", views.RequiresLogin(views.ShowCompleteTasksFunc))
+	muxTracer.HandleFunc(router, "/", views.RequiresLogin(views.ShowAllTasksFunc))
+	muxTracer.HandleFunc(router, "/category/", views.RequiresLogin(views.ShowCategoryFunc))
+	muxTracer.HandleFunc(router, "/deleted/", views.RequiresLogin(views.ShowTrashTaskFunc))
+	muxTracer.HandleFunc(router, "/completed/", views.RequiresLogin(views.ShowCompleteTasksFunc))
 
 	//these handlers perform action like delete, mark as complete etc
-	http.HandleFunc("/complete/", views.RequiresLogin(views.CompleteTaskFunc))
-	http.HandleFunc("/files/", views.RequiresLogin(views.UploadedFileHandler))
-	http.HandleFunc("/trash/", views.RequiresLogin(views.TrashTaskFunc))
-	http.HandleFunc("/edit/", views.RequiresLogin(views.EditTaskFunc))
-	http.HandleFunc("/search/", views.RequiresLogin(views.SearchTaskFunc))
+	muxTracer.HandleFunc(router, "/complete/", views.RequiresLogin(views.CompleteTaskFunc))
+	muxTracer.HandleFunc(router, "/files/", views.RequiresLogin(views.UploadedFileHandler))
+	muxTracer.HandleFunc(router, "/trash/", views.RequiresLogin(views.TrashTaskFunc))
+	muxTracer.HandleFunc(router, "/edit/", views.RequiresLogin(views.EditTaskFunc))
+	muxTracer.HandleFunc(router, "/search/", views.RequiresLogin(views.SearchTaskFunc))
 
-	http.Handle("/static/", http.FileServer(http.Dir("public")))
+	router.Handle("/static/", http.FileServer(http.Dir("public")))
 
-	http.HandleFunc("/api/get-task/", views.GetTasksFuncAPI)
-	http.HandleFunc("/api/get-deleted-task/", views.GetDeletedTaskFuncAPI)
-	http.HandleFunc("/api/add-task/", views.AddTaskFuncAPI)
-	http.HandleFunc("/api/update-task/", views.UpdateTaskFuncAPI)
-	http.HandleFunc("/api/delete-task/", views.DeleteTaskFuncAPI)
+	muxTracer.HandleFunc(router, "/api/get-task/", views.GetTasksFuncAPI)
+	muxTracer.HandleFunc(router, "/api/get-deleted-task/", views.GetDeletedTaskFuncAPI)
+	muxTracer.HandleFunc(router, "/api/add-task/", views.AddTaskFuncAPI)
+	muxTracer.HandleFunc(router, "/api/update-task/", views.UpdateTaskFuncAPI)
+	muxTracer.HandleFunc(router, "/api/delete-task/", views.DeleteTaskFuncAPI)
 
-	http.HandleFunc("/api/get-token/", views.GetTokenHandler)
-	http.HandleFunc("/api/get-category/", views.GetCategoryFuncAPI)
-	http.HandleFunc("/api/add-category/", views.AddCategoryFuncAPI)
-	http.HandleFunc("/api/update-category/", views.UpdateCategoryFuncAPI)
-	http.HandleFunc("/api/delete-category/", views.DeleteCategoryFuncAPI)
+	muxTracer.HandleFunc(router, "/api/get-token/", views.GetTokenHandler)
+	muxTracer.HandleFunc(router, "/api/get-category/", views.GetCategoryFuncAPI)
+	muxTracer.HandleFunc(router, "/api/add-category/", views.AddCategoryFuncAPI)
+	muxTracer.HandleFunc(router, "/api/update-category/", views.UpdateCategoryFuncAPI)
+	muxTracer.HandleFunc(router, "/api/delete-category/", views.DeleteCategoryFuncAPI)
 
 	log.Println("running server on ", values.ServerPort)
-	log.Fatal(http.ListenAndServe(values.ServerPort, nil))
+	log.Fatal(http.ListenAndServe(values.ServerPort, router))
 }
